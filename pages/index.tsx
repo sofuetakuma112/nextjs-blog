3a49e893d14f24873491bc18670d45a8485f70ca
sofue <kawahagi0620@gmail.com>
2022-01-02 12:26:22 +0900

    delete readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,11 +33,7 @@ export default function Home({ allPostsData }: PropsType) {
 			</Head>
 
 			<section className={utilStyles.headingMd}>
-				<p>Nemutas, 雨の日が好きです</p>
-				<p>
-					(This is a sample website - you’ll be building a site like this on{' '}
-					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-				</p>
+				<p>Takuma Sofue</p>
 			</section>
 
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
